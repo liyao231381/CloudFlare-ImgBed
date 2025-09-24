7a54381a90d1b39f2b4fcc2c3b20c7bd399e186c
liyao231381 <2313814221@qq.com>
2025-09-25 04:36:37 +0800

    Update _middleware.js

--- functions/api/_middleware.js
+++ functions/api/_middleware.js
@@ -1,3 +1,11 @@
 import { checkDatabaseConfig } from '../utils/middleware';
 
-export const onRequest = [checkDatabaseConfig];
+const corsMiddleware = async ({ request, next }) => {
+  const response = await next();
+  response.headers.set('Access-Control-Allow-Origin', '*');
+  response.headers.set('Access-Control-Allow-Methods', 'GET,HEAD,POST,PUT,DELETE,OPTIONS');
+  response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  return response;
+};
+
+export const onRequest = [corsMiddleware, checkDatabaseConfig];
