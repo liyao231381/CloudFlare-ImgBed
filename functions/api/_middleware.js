852dc5efb80ec855b2baca403077e2c4ef636281
liyao231381 <2313814221@qq.com>
2025-09-25 04:39:58 +0800

    Update _middleware.js

--- functions/api/_middleware.js
+++ functions/api/_middleware.js
@@ -1,6 +1,18 @@
 import { checkDatabaseConfig } from '../utils/middleware';
 
 const corsMiddleware = async ({ request, next }) => {
+  // 处理预检请求
+  if (request.method === 'OPTIONS') {
+    return new Response(null, {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,HEAD,POST,PUT,DELETE,OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        'Access-Control-Max-Age': '86400', // 缓存预检请求结果一天
+      },
+    });
+  }
+
   const response = await next();
   response.headers.set('Access-Control-Allow-Origin', '*');
   response.headers.set('Access-Control-Allow-Methods', 'GET,HEAD,POST,PUT,DELETE,OPTIONS');
