37293fb8f635853af0755455e56acf19fc17fddb
liyao231381 <2313814221@qq.com>
2025-10-26 00:44:32 +0800

    Implement CORS middleware for handling requests
    Add CORS middleware to handle preflight requests and set headers.

--- functions/dav/_middleware.js
+++ functions/dav/_middleware.js
@@ -1,3 +1,22 @@
 import { checkDatabaseConfig } from '../utils/middleware';
 
+const corsMiddleware = async ({ request, next }) => {
+  if (request.method === 'OPTIONS') {
+    return new Response(null, {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,HEAD,POST,PUT,DELETE,OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        'Access-Control-Max-Age': '86400',
+      },
+    });
+  }
+
+  const response = await next();
+  response.headers.set('Access-Control-Allow-Origin', '*');
+  response.headers.set('Access-Control-Allow-Methods', 'GET,HEAD,POST,PUT,DELETE,OPTIONS');
+  response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  return response;
+};
+
 export const onRequest = [checkDatabaseConfig];
